224bb227c7831d4c83adc3ad258361f2bf111431
Tomasz Kwaśny <tomasz.kwasny@webengineer.biz>
2017-11-28 17:17:55 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+        <!--<link rel="alternate" hreflang="" href="">-->
+        <link rel="shortcut icon" href="/favicon.ico" type="image/png">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <link rel="stylesheet" type="text/css" href="css/reset.css"/>
+        <link rel="stylesheet" type="text/css" href="css/my-css.css"/>
+
+
+    </head>
+    <body>
+        <div class="container">
+            <div class=" row header ">
+                <div class="col--2 left "></div>
+                <div class="col--8 center ">Center</div>
+                <div class="col--2 right "></div>
+            </div>       
+            <div class="row header ">
+                <div class="col--2 left "></div>
+                <div class="col--8 center ">Center</div>
+                <div class="col--2 right "></div>
+            </div>
+            <div class="row footer ">
+                Stopka
+            </div>
+        </div>
+
+        <script src="js/jquery-3.2.1.js"></script>
+        <!--<script src="js/bootstrap.min.js"></script>-->
+    </body>
+</html>
+
